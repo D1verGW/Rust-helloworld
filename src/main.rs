3b578903ac6d3ddce815b0a925f1dd06cc38dc8c
GW <gribok.work@gmail.com>
2020-02-08 00:44:21 +0200

    feat(dev): add hello_world project example
    - simple "match" game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+extern crate rand;
+
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+fn read_number() -> u32 {
+    let stdin = io::stdin();
+    let mut guess = String::new();
+
+    stdin.read_line(&mut guess)
+        .expect("io:stdin().read_line failure");
+
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Input parsing failure, please, try again");
+            return read_number();
+        }
+    };
+
+    return guess;
+}
+
+fn new_try(expected_number: u32) {
+    let guess = read_number();
+
+    println!("You input: {}", guess);
+
+    match guess.cmp(&expected_number) {
+        Ordering::Less => {
+            println!("Too small!");
+            return new_try(expected_number);
+        }
+        Ordering::Greater => {
+            println!("Too big!");
+            return new_try(expected_number);
+        }
+        Ordering::Equal => {
+            println!("Sic! You amazing! Guessed correctly!");
+            return;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let expected_number: u32 = rand::thread_rng().gen_range(1, 101);
+
+    println!("Try to guess number!");
+
+    println!("Please, input you number");
+
+    new_try(expected_number);
 }
